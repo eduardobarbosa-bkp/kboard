26a1ad3c0fb0ed4b508a6f150640cab214985fcd
eduardobarbosa <eduardobarbosadacosta@gmail.com>
2013-07-09 00:08:23 -0300

    config produces request mapping

---
+++ src/main/scala/br/com/ebc/kboard/web/spring/filter/CorsFilter.scala
@@ -0,0 +1,19 @@
+package br.com.ebc.kboard.web.spring.filter
+
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.io.IOException
+
+class CorsFilter extends OncePerRequestFilter {
+  protected def doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+    response.addHeader("Access-Control-Allow-Origin", "*")
+    if (request.getHeader("Access-Control-Request-Method") != null && ("OPTIONS" == request.getMethod)) {
+      response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+      response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept")
+    }
+    filterChain.doFilter(request, response)
+  }
+}
