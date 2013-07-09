2993db380c4de08e096548dfbe80941c81f6291c
eduardobarbosa <eduardobarbosadacosta@gmail.com>
2013-07-08 23:25:47 -0300

    config produces request mapping

--- src/main/scala/br/com/ebc/kboard/web/spring/TarefaController.scala
+++ src/main/scala/br/com/ebc/kboard/web/spring/TarefaController.scala
@@ -4,11 +4,12 @@ package br.com.ebc.kboard.web.spring
 import org.springframework.stereotype.Controller
 import org.springframework.beans.factory.annotation.Autowired
 import br.com.ebc.kboard.model.Tarefa
-import org.springframework.web.bind.annotation.{RequestBody, ResponseBody, PathVariable, RequestMapping}
+import org.springframework.web.bind.annotation._
 import org.springframework.web.bind.annotation.RequestMethod._
 import javax.validation.Valid
 import br.com.ebc.kboard.business.BaseService
 import br.com.ebc.kboard.web.spring.util.BaseController
+import org.springframework.http.HttpStatus
 
 
 @Controller
@@ -19,27 +20,27 @@ class TarefaController extends BaseController{
   private val baseService: BaseService = null
 
   @RequestMapping(value = Array("/save"), method = Array(POST), produces = Array("application/json"))
+  @ResponseStatus(HttpStatus.OK)
   @ResponseBody
   def save(@RequestBody @Valid task: Tarefa) = {
     baseService.save(task)
     task
   }
 
-  @RequestMapping(value = Array("/update"), method = Array(PUT), produces = Array("application/json"))
-  @ResponseBody
-  def update(@RequestBody @Valid task: Tarefa) = {
+  @RequestMapping(value = Array("/update"), method = Array(PUT))
+  def update(@RequestBody @Valid task: Tarefa) {
     baseService.update(task)
-    task
   }
 
-  @RequestMapping(value = Array("/delete/{id}"), method = Array(DELETE), produces = Array("application/json"))
-  def delete(@PathVariable id: String) = {
+  @RequestMapping(value = Array("/delete/{id}"), method = Array(DELETE))
+  def delete(@PathVariable id: String) {
     val task = baseService.findById(classOf[Tarefa], id)
     if(task != null)
       baseService.delete(task.asInstanceOf[Tarefa])
   }
 
   @RequestMapping(value = Array("/find/{id}"), method = Array(GET), produces = Array("application/json"))
+  @ResponseStatus(HttpStatus.OK)
   @ResponseBody
   def find(@PathVariable id: String) =  {
     baseService.findById(classOf[Tarefa], id)
@@ -47,6 +48,7 @@ class TarefaController extends BaseController{
 
 
   @RequestMapping(value = Array("/list"), method = Array(GET), produces = Array("application/json"))
+  @ResponseStatus(HttpStatus.OK)
   @ResponseBody
   def list() =  {
     baseService.findAll(classOf[Tarefa])
